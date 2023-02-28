5e8df8bbf1248dd438cdfe9558573be25c955565
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-28 05:53:52 -0500

    Initial Commit

---
+++ Drawing_Project9/Drawing_Project9App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_Project9App.swift
+//  Drawing_Project9
+//
+//  Created by Dev on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_Project9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
