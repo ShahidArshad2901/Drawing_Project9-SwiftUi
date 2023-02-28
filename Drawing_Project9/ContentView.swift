0e2cc188d85e5140715d559bea9bba6c607c82ae
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-28 11:26:20 -0500

    Special effects in SwiftUI: blurs, blending, and more

--- Drawing_Project9/ContentView.swift
+++ Drawing_Project9/ContentView.swift
@@ -5,45 +5,42 @@
 //  Created by Dev on 28/02/2023.
 //
 
+//
+
+
+
 import SwiftUI
 
-struct ColorCycleCircle: View {
-    var amount = 0.0
-    var steps = 100
-    
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(gradient: Gradient(colors: [color(for: value, brightness: 1),
-                            color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom)
-                        , lineWidth: 2)
-            }
-        }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
     var body: some View {
         VStack {
-            ColorCycleCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
-            Slider(value: $colorCycle)
+            ZStack{
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.yellow)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
